c7b85dd7ad0814f1bba2ca1450f345645f15ede3
EC2 Default User <ec2-user@ip-172-31-17-198.eu-west-1.compute.internal>
2018-05-15 18:17:58 +0000

    heroku endpoints

--- config/initializers/optimizer_endpoints.rb
+++ config/initializers/optimizer_endpoints.rb
@@ -3,10 +3,10 @@ uri_D2 = "http://supersede.es.atos.net:8280/replan_optimizer_v2/replan"
 uri_P1 = "http://platform.supersede.eu:8280/replan_optimizer/replan"
 uri_P2 = "http://platform.supersede.eu:8280/replan_optimizer_v2/replan"
 
-local = "http://localhost:8080/replan_optimizer_v2-0.0.1/replan"
+local = "https://replan-optimizer-llavor.herokuapp.com/replan"
 
 Rails.application.config.x.optimizer_endpoints = [local]
 
-localn = "http://localhost:8080/replan_optimizer_v2-0.0.1/replan-n"
+localn = "https://replan-optimizer-llavor.herokuapp.com/replan-n"
 
 Rails.application.config.x.optimizer_n_endpoints = [localn]
